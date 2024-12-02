d1f53f7dca32082eb2cf9a6becc2fe5b8b019bc9
Fardeen003 <fardeen@arramton.com>
2024-12-02 19:00:23 +0530

    .

--- js/script.js
+++ js/script.js
@@ -17,3 +17,16 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+// Header Responsive menu
+    function navigateTo(target) {
+        // Close the dropdown
+        $('.navbar-collapse').collapse('hide');
+
+        // Use a timeout to allow the dropdown to close before navigating
+        setTimeout(function() {
+            window.location.hash = target; // Navigate to the target
+        }, 300); // Adjust the timeout duration as needed
+    }
+
+    //  Make the links in navbar active acc. to the user scroll
